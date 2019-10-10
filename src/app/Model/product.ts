3234cbe4ce95525b3e4a5523944fad397781f6a1
ProtoTeemo <gewanhce@gmail.com>
2019-10-10 12:43:34 +0300

    Added: ProductComponent
    *added imgUrl to json and model (product)
    *test display with images

--- src/app/Model/product.ts
+++ src/app/Model/product.ts
@@ -2,6 +2,7 @@ export class Product {
     id:number;
     name:string;
     price:number;
+    imgUrl:string;
     prdoucer: string;
     driveType: string;
     desc:string;
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './product-list/product-list.component';
+import { ProductComponent } from './product/product.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent
+    ProductListComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule,
