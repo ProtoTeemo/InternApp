957b5a835d92b51ab484f4a5425a0e6c80b9bef9
ProtoTeemo <gewanhce@gmail.com>
2019-10-12 17:27:24 +0300

    Fix: Filter work

--- src/app/filter.pipe.ts
+++ src/app/filter.pipe.ts
@@ -2,6 +2,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 import { Product } from './Model/product';
 import { FilterViewModel } from './ViewModel/FilterViewModel';
 import { Filter } from './Model/filter';
+import { ProductsService } from './products.service';
 
 @Pipe({
     name: "filter",
@@ -9,18 +10,24 @@ import { Filter } from './Model/filter';
 })
 export class FilterPipe implements PipeTransform{
 
-    transform(products:Product[], filters:{ filter:Filter, selectedOption:string}[]) {
+    transform(products:Product[], filters:{ filter:Filter, selectedOption:string}[]) {        
         if(filters.length > 0){
+            let isCorrect = false;
             let result = products.filter(product => {
                 for (let filter of filters) {
                     switch (filter.filter.propName) {
                         case "producer":
-                            return product.prdoucer == filter.selectedOption;
+                            if(product.prdoucer == filter.selectedOption)
+                                return true;
+                            else
+                                continue;
                         case "driveType":
-                            return product.driveType == filter.selectedOption;
+                            if(product.driveType == filter.selectedOption)
+                                return true;
+                            else
+                                continue;
                         default:
                             return false;
-                            break;
                     }
                 }
             });
