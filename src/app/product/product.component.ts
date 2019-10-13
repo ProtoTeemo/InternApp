e4fd6da86dd1f249a73e72d3861e877e676c2703
ProtoTeemo <gewanhce@gmail.com>
2019-10-13 16:17:28 +0300

    Hover on the product  has shows description

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -12,6 +12,7 @@ export class ProductComponent implements OnInit {
 
   @Input() public product: Product;
   @Input() filterViewModel:FilterViewModel;
+  isShownDesc:boolean = false;
 
   constructor() { }
 
